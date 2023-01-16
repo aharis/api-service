9f0e5169e18257b12d5a3ff05116c56ee9cf384e
Sejfudin <sejfudindu@maestralsolutions.com>
2023-01-16 23:25:07 +0100

    added env file

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 export async function dbConnect() {
     mongoose.set('strictQuery', false)
     try {
-        const mongoUri = 'mongodb://localhost:27017/comerce'
+        const mongoUri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_NAME}.mongodb.net/?retryWrites=true&w=majority`;
         mongoose.connect(mongoUri, {
             autoIndex: true
         }, async () => {
-            console.log('Connected to mongo datebase')
+            console.log(`Connected to mongo datebase, ${mongoUri}`)
         })
     } catch (e) {
         console.log('Error in mongo connection')
